be0de68dd9bf66e6944971e673f307541ab8b283
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-09-07 09:44:57 -0500

    Add: ActionButton to List component and logic

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,14 @@
+import ActionButton from './ActionButton';
 import Card from './Card';
 
-export default function List({ cards, title, wipLimits }) {
+export default function List({ cards, title, status, wipLimits }) {
   return (
     <div className='list'>
       <div>
         <div className='list__header'>
           {title} { wipLimits !== 0 ? `(${wipLimits})` : '' }
         </div>
+        { status === 'TD' && <ActionButton /> }
         { cards.map(userStory => <Card key={userStory.id} />) }
       </div>
     </div>
