f30a332f455930b05702aae0130fbf6f068d47f7
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-09-06 23:14:15 -0500

    Add: Card component

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+export default function Card() {
+  return (
+    <div className='card'>
+      <div className='card__sideMark'></div>
+      <div className="card__header">
+        <div className="card__title">
+          Card
+        </div>
+      </div>
+    </div>
+  );
+}
