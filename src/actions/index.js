95e477446cddcc04dba66684b0c0867e18e3e104
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-09-08 05:17:27 -0500

    Add: Redux actions constants and logic

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export * from './listActions';
+
+export const CONSTANTS = {
+  ADD_CARD: 0,
+  ADD_LIST: 1,
+};
+
---
+++ src/actions/listActions.js
@@ -0,0 +1,6 @@
+export const addList = title => {
+  return {
+    title
+  };
+};
+
