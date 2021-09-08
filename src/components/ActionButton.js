3de669fa2995b927356aa5848890d482308261f3
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-09-07 22:12:53 -0500

    Add: User's story form to Action Button component

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,9 +1,54 @@
+import { useState } from 'react';
+
 export default function ActionButton() {
+  const [showForm, setShowForm] = useState(false);
+
+  const renderForm = () => {
+    return (
+      <div className='card'>
+        <div className="card__form">
+          <input className='card__input' type='text' placeholder="Enter user's story" />
+          <textarea
+            className='card__description'
+            name=''
+            rows='4'
+            placeholder='Description'
+          ></textarea>
+          <select className='card__select'>
+            <option value='0'>Select priority</option>
+            <option value='h'>High</option>
+            <option value='m'>Medium</option>
+            <option value='l'>Low</option>
+          </select>
+          <input
+            className='card__input'
+            type='number'
+            placeholder='Enter hours aprox'
+          />
+          <button
+            className='card__btn--default'
+          >
+            <span>Assign</span>
+          </button>
+          </div>
+        <button
+          className='card__btn--primary'
+        >
+          Add Story
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <button
-      className='actionButton'
-    >
-      Add Story
-    </button>
+    <div>
+      <button
+        className='actionButton'
+        onClick={() => setShowForm(!showForm)}
+      >
+        { showForm ? <span>Discard</span> : <span>Add Story</span> }
+      </button>
+      { showForm && renderForm() }
+    </div>
   );
 }
