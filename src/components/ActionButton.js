5dc54e6fed64af2332c0f1ca79e04a586c17c397
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2021-09-08 07:16:41 -0500

    Add: Redux and actions to ActionButton

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,20 +1,56 @@
 import { useState } from 'react';
 
-export default function ActionButton() {
+import { connect } from 'react-redux';
+import { addList } from '../actions';
+
+function ActionButton(props) {
   const [showForm, setShowForm] = useState(false);
+  const [story, setStory] = useState({
+    title: '',
+    description: '',
+    priority: '0',
+    estimatedHours: 0,
+    peopleInCharge: [],
+    issues: [],
+    comments: []
+  });
+  const {
+    title,
+    description,
+    priority,
+    estimatedHours,
+    peopleInCharge
+  } = story;
+
+  const handleAddList = () => {
+    const { dispatch } = props;
+    const requireData = title && description && priority && estimatedHours;
+
+    if (requireData) {
+
+    }
+  };
 
   const renderForm = () => {
     return (
       <div className='card'>
         <div className="card__form">
-          <input className='card__input' type='text' placeholder="Enter user's story" />
+          <input
+            className='card__input'
+            type='text'
+            name='title'
+            placeholder="Enter user's story"
+          />
           <textarea
             className='card__description'
-            name=''
+            name='description'
             rows='4'
             placeholder='Description'
           ></textarea>
-          <select className='card__select'>
+          <select
+            className='card__select'
+            name='priority'
+          >
             <option value='0'>Select priority</option>
             <option value='h'>High</option>
             <option value='m'>Medium</option>
@@ -23,6 +59,7 @@ export default function ActionButton() {
           <input
             className='card__input'
             type='number'
+            name='estimatedHours'
             placeholder='Enter hours aprox'
           />
           <button
@@ -52,3 +89,5 @@ export default function ActionButton() {
     </div>
   );
 }
+
+export default connect()(ActionButton);
